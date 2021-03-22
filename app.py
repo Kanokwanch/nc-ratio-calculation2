25764eec03ee09ecd29a0a4ed074410ccb84d636
aaomsan <67183130+aaomsan@users.noreply.github.com>
2021-03-22 16:52:02 +0700

    Update app.py

--- app.py
+++ app.py
@@ -261,12 +261,13 @@ def main():
     st.title("NC Ratio Calculation 👩🏼‍⚕️")
     st.write("**Senior Project**")
     st.write(
-    '''
-            **Member** 
-                1. Kanokwan Chasuwan
-                2. Chanistha Krikhajornkitti
+        '''
 
-    ''')
+        **Member** 
+            1. Kanokwan Chasuwan
+            2. Chanistha Krikhajornkitti
+
+        ''')
     activities = ["Home", "About"]
     choice = st.sidebar.selectbox("Menu", activities)
    
